c439aee97ea8fe96e42c968833b22bf0db40d6e7
Jinghua Feng <32555880+tracygooo@users.noreply.github.com>
2020-05-02 21:31:44 -0400

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE covid19 (
+    TestDate DATE, 
+    County VARCHAR(20), 
+    NewPositives INTEGER, 
+    CumulativeNumberOfPositives INTEGER, 
+    TotalNumberOfTestsPerformed INTEGER,
+    CumulativeNumberOfTestsPerformed INTEGER
+)；
