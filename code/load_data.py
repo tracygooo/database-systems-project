06c7ccd66bb334585668c86da9a1ac371b2ff50e
tracygooo <tracygooo@gmail.com>
2020-05-03 23:17:29 -0400

    Finished data loading

--- code/load_data.py
+++ code/load_data.py
@@ -4,6 +4,7 @@ import psycopg2.extras
 import os
 from datetime import datetime
 
+# --- Create schema
 schema_fname = 'schema.sql'
 connect_string = "host='localhost' dbname='dbms_final_project' user='dbms_project_user' password='dbms_password'"
 conn = psycopg2.connect( connect_string )
@@ -12,16 +13,13 @@ create_schema = open( schema_fname , 'r' ).read()
 cursor.execute( create_schema )
 conn.commit()
 
-dataset_fname= 'datasets/covid19.csv'
-table_name = 'covid19'
-f_dataset = open( dataset_fname , "r")
-cursor.copy_expert( "copy {} from STDIN CSV HEADER QUOTE '\"'".format( table_name ), f_dataset )
-conn.commit()
-
-dataset_fname= 'datasets/weather.csv'
-table_name = 'weather_tmp'
-f_dataset = open( dataset_fname , "r")
-print( "\n'{}' loading table: {}".format( table_name , datetime.now() ) )
-cursor.copy_expert( "copy {} from STDIN CSV HEADER QUOTE '\"'".format( table_name ), f_dataset )
-conn.commit()
-print( "\n'{}' completing loading table: {}".format( table_name , datetime.now() ) )
+# --- Load data into tables 
+dataset_path = 'datasets/'
+dataset_fname = [ 'covid19.csv' , 'weather.csv' , 'crash.csv' ]
+table_name = [ 'covid19' , 'weather' , 'crash' ]
+for i in range( len(dataset_fname) ) :
+    print( "\nTable '{}' start loading: {}".format( table_name[i] , datetime.now() ) )
+    f_dataset = open( dataset_path + dataset_fname[i] , "r")
+    cursor.copy_expert( "copy {} from STDIN CSV HEADER QUOTE '\"'".format( table_name[i] ), f_dataset )
+    conn.commit()
+    print( "\nTable '{}' completing loading: {}".format( table_name[i] , datetime.now() ) )
