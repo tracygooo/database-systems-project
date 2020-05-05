696d08e7931d88b3dcb060c30dfb3399d31970c9
tracygooo <tracygooo@gmail.com>
2020-05-05 12:51:37 -0400

    Setup covid19-1 button and query

--- code/user_interface2.py
+++ code/user_interface2.py
@@ -11,21 +11,22 @@ class Window(Frame):
         Frame.__init__(self, master)
         self.master = master
 
-def submitCovidByDateCounty( test_date , county , records ):
+def submitCovidByDateCounty( test_date , county , frame , myrow ):
     connect_string = "host='localhost' dbname='dbms_final_project' user='dbms_project_user' password='dbms_password'"
     conn = psycopg2.connect( connect_string )
     cursor = conn.cursor()
     query = "SELECT * FROM covid19 WHERE testdate = %s and county ILIKE %s"
     cursor.execute( query , ( test_date.get() , county.get() ) )
-    records.set( cursor.fetchall()[0] )
-    print(records.get())
+    records = cursor.fetchall()[0]
+    records_label = Label( frame , text = records )
+    records_label.grid( row = myrow + 1 , column = 1 , columnspan = 2 )
     conn.commit()
 
     print_records = "Date \t County \t New Positive \t Total tested\n"
     #for record in records :
     #    print_records = print_records + str( record ) + '\n'
-    print( records )
     # records_label.pack()
+    print(records)
 
 def getChildren (window) :
     child_list = window.winfo_children()
@@ -39,29 +40,30 @@ def clearChildren( window ) :
     for item in widget_list:
         item.grid_forget()
 
-def queryCovidByDateCounty( frame , records ):
+def queryCovidByDateCounty( frame ) :
     clearChildren( frame )
 
-    # Create text labels
+    # Create text labels and entry boxes
+    myrow = 0
     date_label = Label( frame , text = "Date" )
-    date_label.grid( row = 0 , column = 1 )
-    county_label = Label( frame , text = "County" )
-    county_label.grid( row = 1 , column = 1 )
+    date_label.grid( row = myrow , column = 0 )
+    date_entry = Entry( frame , width = 30 )
+    date_entry.grid( row = myrow , column = 1  )
+    myrow += 1
 
-    # Create text boxes
-    test_date = Entry( frame , width = 30 )
-    test_date.grid( row = 0 , column = 2  )
-    county = Entry( frame , width = 30 )
-    county.grid( row = 1 , column = 2 )
+    county_label = Label( frame , text = "County" )
+    county_label.grid( row = myrow , column = 0 )
+    county_entry = Entry( frame , width = 30 )
+    county_entry.grid( row = myrow , column = 1 )
+    myrow += 1
 
     # Create submit button
-    my_submit = Button( frame , text = "Submit" , command = partial( submitCovidByDateCounty , test_date , county , records ) )
-    my_submit.grid( row = 2 , column = 2 , columnspan = 2 , pady = 10 , padx = 10, ipadx = 100 )
+    my_submit = Button( frame , text = "Submit" , command = partial( submitCovidByDateCounty , date_entry , county_entry , frame , myrow ) )
+    my_submit.grid( row = myrow , column = 1 , columnspan = 2 , pady = 10 , padx = 10, ipadx = 100 )
 
-def queryCovidRankCases( frame , records ):
+def queryCovidRankCases( frame ) :
     clearChildren( frame )
     return True
-
 
 def weatherQuery( root ):
     return True
@@ -84,28 +86,25 @@ def crashCovidQuery( root ):
 # --- Setup window 
 root = Tk()
 app = Window(root)
-root.wm_title("Database Systems Final Project")
+root.wm_title( "Database Systems Final Project" )
 root.geometry( "600x400" )
 
 leftframe = Frame(root)
-leftframe.pack(side = LEFT)
+leftframe.pack( side = LEFT )
 
-rightframe = Frame(root)
-rightframe.pack(side = RIGHT)
+rightframe = Frame( root )
+rightframe.pack( side = RIGHT )
 
 # --- Buttons linked to queries 
 mypadx , mypady = 5 , 5 
 mysticky = "E"
 myrow = 0
-records = StringVar()
-records_label = Label( rightframe , textvariable = records )
-records_label.grid( row = 3 , column = 2 , columnspan = 2 )
 
-covid_button1 = Button( leftframe , text = "Covid19-1" , command = partial( queryCovidByDateCounty , rightframe , records ) )
+covid_button1 = Button( leftframe , text = "Covid19-1" , command = partial( queryCovidByDateCounty , rightframe ) )
 covid_button1.grid( row = myrow , column = 0 , sticky = mysticky , padx = mypadx , pady = mypady )
 myrow += 1
 
-covid_button2 = Button( leftframe , text = "Covid19-2" , command = partial( queryCovidRankCases , rightframe , records ) )
+covid_button2 = Button( leftframe , text = "Covid19-2" , command = partial( queryCovidRankCases , rightframe ) )
 covid_button2.grid( row = myrow , column = 0 , sticky = mysticky , padx = mypadx , pady = mypady )
 myrow += 1
 
