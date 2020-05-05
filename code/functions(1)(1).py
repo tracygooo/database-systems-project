91f3ca88d793a4fa3a75e00d7e01d9adae288d43
Jinghua Feng <32555880+tracygooo@users.noreply.github.com>
2020-05-05 15:54:25 -0400

    Update functions(1)(1).py

--- code/functions(1)(1).py
+++ code/functions(1)(1).py
@@ -6,26 +6,25 @@ class functions():
     # rank number of new positive cases in a specified period
     def rank_new_positive(self, from_date, to_date, limit):        
         query = '''SELECT county, SUM(newpositives) 
-        FROM covid19 WHERE testdate > '%s' AND testdate < '%s' 
+        FROM covid19 WHERE testdate > %s AND testdate < %s 
         GROUP BY county 
-        ORDER BY sum(newpositives) DESC LIMIT '%s';'''%(from_date,to_date,limit)
+        ORDER BY sum(newpositives) DESC LIMIT %s;'''
         with self.conn.cursor() as cursor:
-            cursor.execute(query)
+            cursor.execute(query,(str(from_date),str(to_date),str(limit)))
             return cursor.fetchall()
     def positive_ratio(self, from_date, to_date, county):
-        
         query = '''
         SELECT cast(sum(newpositives)::NUMERIC/sum(totalnumberoftestsperformed) as decimal(10,2))
         FROM covid19
-        WHERE TestDATE > '%s' 
-        AND TestDATE < '%s'
-        AND County = '%s';       
-        '''%(from_date,to_date,county)
+        WHERE TestDATE > %s 
+        AND TestDATE < %s
+        AND County = %s;       
+        '''
         with self.conn.cursor() as cursor:
-            cursor.execute(query)
+            cursor.execute(query,(str(from_date),str(to_date),str(county)))
             return cursor.fetchall()[0][0]
         
-    #rank crash factor frequencies   
+    #rank the most common crash factors    
     def rank_crash_factor(self,limit):        
         query = '''SELECT factor1,num1+num2+num3+num4+num5 num
         FROM
@@ -49,13 +48,17 @@ class functions():
         AND factor2 = factor3
         AND factor3 = factor4
         AND factor4 = factor5
-        ORDER BY num DESC LIMIT '%s';'''%(limit)
+        ORDER BY num DESC LIMIT %s;'''
         with self.conn.cursor() as cursor:
-            cursor.execute(query)
+            cursor.execute(query,(str(limit),))
             return cursor.fetchall()    
         
     #crash per day in each weather
     def crash_weather(self, weather):
+        l =['is_foggy','is_foggy_heavy','is_thunder','is_ice_pellets','is_glaze_rime','is_smoke_haze', 'is_damaging_wind','is_mist', 'is_drizzle', 'is_rainy', 'is_snowy', 'is_unknown_precipitation',  'is_freezing_foggy']
+        for e in l:
+            if weather in e:
+                weather = e
         query = '''SELECT num1/num2 ratio
         FROM(
         SELECT count(measure_date) num1
@@ -83,8 +86,6 @@ if __name__ == '__main__':
     L = funcs.rank_crash_factor(10)
     print(L)
     print()
-    L = funcs.crash_weather('is_snowy')
+    L = funcs.crash_weather('snow')
     print('average num of crash in a snowy day is:'+str(L))
     print()
-    
-    
